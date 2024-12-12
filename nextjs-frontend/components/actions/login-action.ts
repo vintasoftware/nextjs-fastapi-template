5ab576cd4071b6a369fcab508c44af87d3fdf191
Anderson Resende <anderson@vinta.com.br>
2024-12-12 19:34:55 -0300

    p

--- nextjs-frontend/components/actions/login-action.ts
+++ nextjs-frontend/components/actions/login-action.ts
@@ -1,7 +1,7 @@
 "use server";
 
-import axios from 'axios';
-// import { hello } from "@/app/clientService";
+// import axios from 'axios';
+import { hello } from "@/app/clientService";
 import { redirect } from "next/navigation";
 import { loginSchema } from "@/lib/definitions";
 
@@ -18,36 +18,36 @@ export async function login(prevState: {}, formData: FormData) {
   // const { username, password } = validatedFields.data;
 
 
-  const name = "Anderson";
-
-  try {
-    const response = await axios.post('https://nextjs-fastapi-template-backend.vercel.app/hello', { name }, {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-
-    console.log('Response:', response.data);
-  } catch (error) {
-
-    console.error('Error:', error);
-    return { message: "failed" };
-  }
-
-  redirect(`/dashboard`);
-
-
-  // const input = {
-  //   body: {
-  //    name: "Anderson"
-  //   },
-  // };
-  // const { error } = await hello(input);
-  // console.log("Request payload:", input);
-  // console.log("Request payload:", error);
-  // if (error) {
+  // const name = "Anderson";
+  //
+  // try {
+  //   const response = await axios.post('https://nextjs-fastapi-template-backend.vercel.app/hello', { name }, {
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //   });
+  //
+  //   console.log('Response:', response.data);
+  // } catch (error) {
+  //
+  //   console.error('Error:', error);
   //   return { message: "failed" };
   // }
-  // // (await cookies()).set("accessToken", data.access_token);
+  //
   // redirect(`/dashboard`);
+
+
+  const input = {
+    body: {
+     name: "Anderson"
+    },
+  };
+  const { error } = await hello(input);
+  console.log("Request payload:", input);
+  console.log("Request payload:", error);
+  if (error) {
+    return { message: "failed" };
+  }
+  // (await cookies()).set("accessToken", data.access_token);
+  redirect(`/dashboard`);
 }
