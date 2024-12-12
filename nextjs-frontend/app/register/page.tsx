950c6ce46c6823c5a163310e3c953e2862af7a44
Anderson Resende <anderson@vinta.com.br>
2024-12-12 17:42:36 -0300

    t

--- nextjs-frontend/app/register/page.tsx
+++ nextjs-frontend/app/register/page.tsx
@@ -10,18 +10,43 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
-import { register } from "@/components/actions/register-action";
-import { useActionState } from "react";
+// import { register } from "@/components/actions/register-action";
+// import { useActionState } from "react";
 import { SubmitButton } from "@/components/ui/submitButton";
 import Link from "next/link";
+import { registerRegister } from "@/app/clientService";
 
-const initialState = { message: "" };
+// const initialState = { message: "" };
 
 export default function Page() {
-  const [state, dispatch] = useActionState(register, initialState);
+  // const [state, dispatch] = useActionState(register, initialState);
+
+  const handleSubmit = (e: React.SyntheticEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("submitted");
+
+    // const body = e.target.body.value;
+    //
+    // fetch("http://localhost:3001/comments", {
+    //   method: "POST",
+    //   body: JSON.stringify(body)
+    // }).then((response) => {
+    //   console.log(response);
+    //   return response.json(); // do something with response JSON
+    // }
+
+    const input = {
+    body: {
+      email: "an@an.com",
+      password: "P123232322#",
+      },
+    };
+    registerRegister(input);
+  };
+
   return (
     <div className="flex h-screen w-full items-center justify-center px-4">
-      <form action={dispatch}>
+      <form onSubmit={handleSubmit}>
         <Card className="w-full max-w-sm">
           <CardHeader>
             <CardTitle className="text-2xl">Signup</CardTitle>
@@ -40,25 +65,25 @@ export default function Page() {
                 required
               />
             </div>
-            {state.errors?.email && (
-              <p className="text-green-600">{state.errors.email}</p>
-            )}
+            {/*{state.errors?.email && (*/}
+            {/*  <p className="text-green-600">{state.errors.email}</p>*/}
+            {/*)}*/}
             <div className="grid gap-2">
               <Label htmlFor="password">Password</Label>
               <Input id="password" name="password" type="password" required />
             </div>
-            {state?.errors?.password && (
-              <div>
-                <p>Password must:</p>
-                <ul>
-                  {state.errors.password.map((error) => (
-                    <li key={error}>- {error}</li>
-                  ))}
-                </ul>
-              </div>
-            )}
+            {/*{state?.errors?.password && (*/}
+            {/*  <div>*/}
+            {/*    <p>Password must:</p>*/}
+            {/*    <ul>*/}
+            {/*      {state.errors.password.map((error) => (*/}
+            {/*        <li key={error}>- {error}</li>*/}
+            {/*      ))}*/}
+            {/*    </ul>*/}
+            {/*  </div>*/}
+            {/*)}*/}
             <SubmitButton text={"sign up"} />
-            <div>{state?.message && <p>{state.message}</p>}</div>
+            {/*<div>{state?.message && <p>{state.message}</p>}</div>*/}
             <div className="mt-4 text-center text-sm">
               <Link href="/login" className="underline">
                 Back to login
