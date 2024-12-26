d545e1c1846575fccecaeac52a5abfd656a75643
Anderson Resende <anderson@vinta.com.br>
2024-12-26 16:49:47 -0300

    m

--- fastapi_backend/app/database.py
+++ fastapi_backend/app/database.py
@@ -7,7 +7,14 @@ from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_asyn
 from .config import settings
 from .models import Base, User
 
-engine = create_async_engine(settings.DATABASE_URL)
+engine = create_async_engine(
+    settings.DATABASE_URL,
+    pool_pre_ping=True,  # 1. Validates connections before use
+    pool_size=1,  # 2. Maximum number of permanent connections
+    max_overflow=0,  # 3. Maximum number of additional connections
+    pool_recycle=1800,  # 4. Maximum age of a connection in seconds
+    echo=False,
+)
 
 async_session_maker = async_sessionmaker(
     engine, expire_on_commit=settings.EXPIRE_ON_COMMIT
