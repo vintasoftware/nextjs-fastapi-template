831c94d1f7200dc9a29c9de326170a89cabb5b9b
Rai Siqueira <rai93siqueira@gmail.com>
2025-08-25 17:22:40 -0300

    chore(fe): update ESLint config file

--- nextjs-frontend/eslint.config.mjs
+++ nextjs-frontend/eslint.config.mjs
@@ -4,11 +4,11 @@ const compat = new FlatCompat({
   baseDirectory: import.meta.dirname,
 });
 
-const eslintConfig = [
-  ...compat.config({
-    extends: ["next/core-web-vitals", "next/typescript", "prettier"],
-    rules: { "@typescript-eslint/no-empty-object-type": "off" },
-  }),
-];
+const eslintConfig = [{
+  ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
+}, ...compat.config({
+  extends: ["next/core-web-vitals", "next/typescript", "prettier"],
+  rules: { "@typescript-eslint/no-empty-object-type": "off" },
+})];
 
 export default eslintConfig;
